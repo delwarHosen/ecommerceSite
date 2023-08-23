eda18f1e69c3f93a687a91e810ff2e8099b45cbf
delwar-hosen <86364023+delwar-hosen@users.noreply.github.com>
2023-08-23 10:27:43 +0600

    added navbar and load data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log("connected")
+const loadProduct = async () => {
+    const loadData = await fetch("https://fakestoreapi.com/products")
+    const data = await loadData.json()
+    return data
+}
+
+const setAllMenu = async () => {
+    const data = await loadProduct();
+    const menu = document.getElementById('menu-item')
+    const uniqueArray = [];
+    for (const product of data) {
+        console.log(product.category)
+        if (uniqueArray.indexOf(product.category) === -1) {
+            uniqueArray.push(product.category);
+            const li = document.createElement('li')
+            li.innerHTML = `<a>${product.category}</a>`
+            menu.appendChild(li)
+        }
+    }
+}
+
+setAllMenu()
